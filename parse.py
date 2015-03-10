a175a458892a624ee2801b9b562f22cb552d277b
David Eads <davideads@gmail.com>
2015-03-10 17:11:49 -0400

    try a while loop wip

--- parse.py
+++ parse.py
@@ -11,25 +11,34 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('ucr-parser')
 
 def parse(file_path):
+    output = []
+    line = True
     with open(file_path) as f:
+        while line:
+            line = f.readline()
+            logger.info(line)
+        
         # Skip to first section
-        for line in f:
-            if SECTION_BREAK in line:
-                logger.info('Found first data line, processing')
-                break
+        #for line in f:
+            #if SECTION_BREAK in line:
+                #logger.info('Found first data line, processing')
+                #break
 
-        for line in f:
-            if line.startswith(END_BREAK):
-                logger.info('Found last data line, done')
-                break
-            if SECTION_BREAK not in line:
-                process_line(line)
+        #for line in f:
+            #if line.startswith(END_BREAK):
+                #logger.info('Found last data line, done')
+                #break
+
+            
+            #line = re.sub(' +', ' ', line).strip()
+            #logger.info(line)
 
 
-def process_line(line):
-    line = re.sub(' +', ' ', line).strip()
 
-    logger.info(line)
+#def process_line(line):
+    #line = re.sub(' +', ' ', line).strip()
+
+    #logger.info(line)
 
 
 if __name__ == '__main__':
